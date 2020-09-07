4668a802084b23c021460701e7676c1758918588
Warinya <wariroja@gmail.com>
2020-09-06 21:49:39 -0700

    added search bar and some designs

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -6,8 +6,7 @@ class SearchResults extends React.Component {
   render() {
     return (
       <div className='SearchResults'>
-        <h2>Results</h2>
-        {/* <!-- Add a TrackList component --> */}
+        <h2>Results for </h2>
       </div>
     );
   }
